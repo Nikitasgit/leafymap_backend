effad4808036ae7d7c321f099688bf45b446042a
Victor Leman <victorleman1@gmail.com>
2025-09-16 11:10:02 +0200

    fixing adding image too large issue and cors

--- app.ts
+++ app.ts
@@ -23,12 +23,16 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://spotlight-project.vercel.app",
+      "https://api.server.innovastay.fr",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
