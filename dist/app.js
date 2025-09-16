6d0210a3bb514b76e7fe2bc6f69f61c5b4bbd95f
Victor Leman <victorleman1@gmail.com>
2025-09-16 10:14:21 +0200

    fixed url origin

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -15,20 +13,16 @@ const categorieRoutes_1 = __importDefault(require("./routes/categorieRoutes"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const placeRoutes_1 = __importDefault(require("./routes/placeRoutes"));
 const eventRoutes_1 = __importDefault(require("./routes/eventRoutes"));
-const partnershipRoutes_1 = __importDefault(
-  require("./routes/partnershipRoutes")
-);
+const partnershipRoutes_1 = __importDefault(require("./routes/partnershipRoutes"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const imageRoutes_1 = __importDefault(require("./routes/imageRoutes"));
 dotenv_1.default.config();
 (0, db_1.default)();
 const app = (0, express_1.default)();
-app.use(
-  (0, cors_1.default)({
-    origin: ["http://localhost:3000", "https://spotlight-project.vercel.app/"],
+app.use((0, cors_1.default)({
+    origin: ["http://localhost:3000", "https://spotlight-project.vercel.app"],
     credentials: true,
-  })
-);
+}));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/api/users", userRoutes_1.default);
@@ -40,11 +34,11 @@ app.use("/api/images", imageRoutes_1.default);
 app.use("/api/partnerships", partnershipRoutes_1.default);
 app.use(errorHandler_1.default);
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: "Something went wrong!",
-    error: process.env.NODE_ENV === "development" ? err.message : undefined,
-  });
+    console.error(err.stack);
+    res.status(500).json({
+        success: false,
+        message: "Something went wrong!",
+        error: process.env.NODE_ENV === "development" ? err.message : undefined,
+    });
 });
 exports.default = app;
