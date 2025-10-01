3fa2e8d308e607eb94a80f022db44a435833ace2
Victor Leman <victorleman1@gmail.com>
2025-10-01 11:54:07 +0200

    deleted export of unexisting models

--- types/models/index.ts
+++ types/models/index.ts
@@ -1,10 +1,7 @@
-
 export * from "./user";
 export * from "./place";
 export * from "./event";
 export * from "./category";
 export * from "./subCategory";
 export * from "./placeCategory";
-export * from "./collaborator";
-export * from "./application";
-export * from "./message";
+export * from "./partnership";
